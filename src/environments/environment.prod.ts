314c40a768d0b63dccbd9abb8c6b8a22b7e19d24
Nathan Pickard <nathan.pickard87@gmail.com>
2020-11-09 17:43:49 -0800

    Environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  OPENWEATHERMAP_API_KEY: 'cca81332aa416bea35d977c1e43d68f7',
+  OPENWEATHERMAP_URL: 'https://api.openweathermap.org/data/2.5/'
 };
